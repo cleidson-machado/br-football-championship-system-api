c32564c96a5506c6c4baca5634c8a77d91650eb0
Cleidson <cleidson.mac@outlook.com>
2023-11-02 19:59:31 -0400

    Error When Try use New Architecture

--- src/core/event-main/championship/championship.module.ts
+++ src/core/event-main/championship/championship.module.ts
@@ -1,16 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ChampionshipService } from './championship.service';
 import { ChampionshipController } from './championship.controller';
 import { oracleDataBaseModule } from 'src/dataBase/oracleDataBase.module';
 import { championshipProviders } from './championship.providers';
-import { TypeOrmExModule } from 'src/architecture/typeorm-ex.module';
-import { Championship } from './entities/championship.entity';
 
 @Module({
-  imports: [
-    oracleDataBaseModule,
-    TypeOrmExModule.forCustomRepository([Championship]),
-  ],
+  imports: [oracleDataBaseModule],
   controllers: [ChampionshipController],
   providers: [...championshipProviders, ChampionshipService],
 })
